5eacdbee9a0b47925020558d2349196c66617fbb
fabio <fabiomarioluna@gmail.com>
2022-07-03 20:31:24 -0300

    empiezo a querer subir la suma a migithub

---
+++ suma.java
@@ -0,0 +1,10 @@
+public class suma {
+    public static void main(String[] args) {
+        int numUno = 5;
+        int numDos = 7;
+        int resultado = 0;
+
+        resultado = numUno + numDos;
+        System.out.println(resultado);
+    }
+}
