8e5a96a300c6b68258a0bda757e48dd67daad2d1
fabio <fabiomarioluna@gmail.com>
2022-07-03 21:46:43 -0300

    modifiqué la rama suma2

--- suma.java
+++ suma.java
@@ -1,7 +1,7 @@
 public class suma {
     public static void main(String[] args) {
-        int numUno = 5;
-        int numDos = 7;
+        int numUno = 555;
+        int numDos = 77777;
         int resultado = 0;
 
         resultado = numUno + numDos;
