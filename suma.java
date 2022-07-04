c3bbd559273191c795bc21ae318d03b03c072eda
FabioLunaa <95083908+FabioLunaa@users.noreply.github.com>
2022-07-03 21:54:35 -0300

    agregué los 8s

--- suma.java
+++ suma.java
@@ -2,6 +2,7 @@ public class suma {
     public static void main(String[] args) {
         int numUno = 555;
         int numDos = 77777;
+        int numTres = 888888;
         int resultado = 0;
 
         resultado = numUno + numDos;
